93e1e7648bd9b5d4e7c8f308b552e82d811b9c40
vishal1127 <vishal.pb.27@gmail.com>
2025-02-11 15:28:00 +0000

    URL helpers-link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  # ////////////////////////////my code/////////////////////////////
+  #'/' is optional in paths
   # GET /about
-  get "about", to: "about#index"
+  get "about-us", to: "about#index", as: :about
 
-  get "/", to: "main#index"
+  root "main#index"  #can use root instead of get "/", to: "main#index"
 end
